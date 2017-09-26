a159a9692531a72f027e0e6a6bf7c7bbeead4963
Jacob C Moore <jmooree30@gmail.com>
2017-09-26 00:18:02 -0400

    Cleaned up syntax

--- knights_travels.rb
+++ knights_travels.rb
@@ -1,10 +1,10 @@
 class Knight 
-      attr_accessor :path, :x, :y, :moves
-      def initialize(position, path = [])
-        @path = path + [position]
-        @x = position[0]
-        @y = position[1]
-        @moves = [
+  attr_accessor :path, :x, :y, :moves
+  def initialize(position, path = [])
+    @path = path + [position]
+    @x = position[0]
+    @y = position[1]
+    @moves = [
             [-1,-2],
             [-2,-1],
             [-2,+1],
@@ -13,39 +13,39 @@ class Knight
             [+2,-1],
             [+2,+1],
             [+1,+2]]
-    end
+  end
 
-    def possible
-      move_list = Array.new
-      @moves.each do |moves| 
-        x = @x + moves[0] 
-        y = @y + moves[1]
-          if x.between?(0,7)  
-            if y.between?(0,7)
-            move_list << [x,y]  
-            end
-          end 
+  def possible
+    move_list = Array.new
+    @moves.each do |moves| 
+      x = @x + moves[0] 
+      y = @y + moves[1]
+      if x.between?(0,7)  
+        if y.between?(0,7)
+          move_list << [x,y]  
+        end
       end 
-       move_list
-      end 
-
     end 
+    move_list
+  end 
 
-    def shortest_path(position,goal)
-      paths = Array.new
-      @start_knight = Knight.new(position)
-        until
-          @start_knight.x == goal[0] and
-          @start_knight.y == goal[1]
-          @start_knight.possible.each do |moves| 
-            paths << Knight.new(moves,@start_knight.path)
-          end
-          shifted = paths.shift
-          @start_knight = shifted
-        end 
-        counter = @start_knight.path.size 
-          puts "You made it in #{counter} moves! Here's your path:\n"
-            @start_knight.path.each do |path| print path end 
+end 
+
+  def shortest_path(position,goal)
+    paths = Array.new
+    @start_knight = Knight.new(position)
+    until
+      @start_knight.x == goal[0] and
+      @start_knight.y == goal[1]
+      @start_knight.possible.each do |moves| 
+        paths << Knight.new(moves,@start_knight.path)
+      end
+      shifted = paths.shift
+      @start_knight = shifted
+    end 
+    counter = @start_knight.path.size 
+    puts "You made it in #{counter} moves! Here's your path:\n"
+    @start_knight.path.each do |path| print path end 
     end
 
     shortest_path([0,0],[7,7])
